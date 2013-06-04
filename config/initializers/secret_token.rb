d71dd219d45c2e4ec381ff5057f9a882b2828794
helix-dan <lotus.bat@gmail.com>
2013-06-04 15:31:41 +0800

    add new file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsSampleExpo::Application.config.secret_token = 'b1be756081d2e7fd7d7b9f266a668b7e1768d85acaa091547651274218ddad354ead670002cd15ff7e2fea1fc74ac16527a0829450dc1d880b54a48d5ffcd423'
