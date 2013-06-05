ad1356e43df4d0fdd0bbaa414cd6f3000d80220c
helix-dan <lotus.bat@gmail.com>
2013-06-05 19:06:26 +0800

    clear bdd code

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,49 +3,45 @@ require 'spec_helper'
 describe "Static pages \'s" do
 
   describe "Home page" do
+  	before { visit sp_home_path }
     it "should have the content 'Sample App'" do
-      visit sp_home_path
       page.should have_content('Sample App')
     end
 
     it "should have a home title" do
-      visit sp_home_path
       page.should have_selector('title', :text => 'Home')
     end
   end
 
   describe "Help page" do
+  	before { visit sp_help_path }
     it "should have a help page" do
-      visit sp_help_path
       page.should have_content('Help Page')
     end
 
-    it "should have a help title" do 
-      visit sp_help_path
+    it "should have a help title" do
       page.should have_selector('title', :text => "Help")
     end
   end
 
   describe "About page" do
+  	before { visit sp_about_path }
     it "should be a about page" do
-      visit sp_about_path
       page.should have_content('About Page')
     end
 
     it "should be a fuck title" do
-      visit sp_about_path
       page.should have_selector('title', :text => "FUCK")
     end
   end
 
   describe "Content page" do
+  	before { visit sp_content_path }
     it "should be a content page" do
-      visit sp_content_path
       page.should have_content('Content Page')
     end
 
     it "should have a content title" do
-      visit sp_content_path
       page.should have_selector('title', :text => "Content")
     end
   end
